8199238f6089dcf3d7f54b8c2eb3ee9d1f83f330
SethFarrell <sethfarell@ayhoo.com>
2024-06-28 16:10:09 -0700

    cleaned up, improve container creation

--- scripts/create_container.sh
+++ scripts/create_container.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # Run "xhost +local:docker" in terminal before using launch script otherwise you cannot visualize applications in container
 # docker run -it --net=host --env=DISPLAY "mount volumes" "image name" 
+
+# Example Usage:
+# bash create_container.sh ros:humble-perception test_container_name ~/ yes
+if [ $1 = "-h" ]
+then
 echo "######################################################################"
 echo "############################ Usage ###################################"
 echo "######################################################################"
-echo "### bash launch_script.sh image_name volume_name use_gpus [yes/no] ###"
+echo "### bash create_container.sh image_name container_name path_to_mount_in_container use_gpus[yes/no] ###"
 echo "######################################################################"
 echo "######################################################################"
-
-if [ $1 = "-h" ]
-then
 exit
 fi
 
@@ -18,23 +20,47 @@ xhost +local:docker;
 
 # Get arguments from command line
 image_name=$1;
-volume_to_mount=$2;
+container_name=$2
+volume_to_mount=$3;
+
+# Check if the container already exists and remove it if it does
+if [ "$(docker ps -aq -f name=$container_name)" ]; then
+    echo "Container with name $container_name already exists. Removing it..."
+    docker stop "$container_name" && docker rm "$container_name"
+fi
+
 
 use_gpus="--gpus all";
-if [ $3 = "no" ]
+if [ $4 = "no" ]
 then
 use_gpus="";
 fi
 
 clear
 
+
+docker run -d -v $volume_to_mount:/mounted_volume $use_gpus --net=host --env=DISPLAY -v /dev:/dev --group-add dialout --privileged=true --restart=always -w /mounted_volume --name $container_name $image_name sh -c "while true; do sleep 1000; done" && {
+    # docker 
+    docker exec -it "$container_name" bash -c "echo 'Container Created'; exit"
+    # docker wait "$container_name"
+    # docker stop "$container_name"
+} || {
+    echo "Error: Docker run command failed"
+    xhost -local:docker;
+    exit 1
+}
+
+clear
+
 echo "######################################################################"
 echo "################## You are now inside a container ####################"
 echo "################## Image: $image_name "
-echo "################## GPUS: $3 ##########################################"
+echo "################## Container: $container_name "
+echo "################## GPUS: $4 #########################################"
 echo "######################################################################"
 
-docker run -it -v $volume_to_mount:/mounted_volume $use_gpus --net=host --env=DISPLAY -v /dev:/dev --group-add dialout --privileged $image_name
+docker exec -it $container_name bash
+
 
 echo "######################################################################"
 echo "###################### Exiting Container #############################"
